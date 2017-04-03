a262d77eb504d3d50ee356b9f8eba0150ef377aa
Salvatore Laisa <moebiusmania@gmail.com>
2017-04-03 17:30:49 +0200

    fixed syntax for bugsnag-sourcemaps v0.0.7

--- upload.js
+++ upload.js
@@ -1,23 +1,26 @@
+#!/usr/bin/env node
+
 'use strict';
 
 const PATH = require('path');
-const ROOT = require('find-root')
-const UPLOAD = require('bugsnag-sourcemap').upload;
-const PROJECT = require(ROOT(PATH.resolve(__dirname)) + '/package.json');
-
-// console.log(PROJECT);
-console.log(`BUGSNAG: uploading sourcemap for v${PROJECT.version}`);
-
-UPLOAD({
+const ROOT = require('find-root');
+const UPLOAD = require('bugsnag-sourcemaps').upload;
+const PROJECT = require(ROOT(PATH.resolve(__dirname, './../')) + '/package.json');
+const OPTIONS = {
+  endpoint: 'https://upload-bugsnag.contactlab.it/',
   apiKey: PROJECT.bugsnag.apiKey,
   appVersion: PROJECT.version,
   minifiedUrl: PROJECT.bugsnag.minifiedUrl,
-  sourceMap: PATH.resolve(__dirname, '../app/bundle.js.map'),
-  minifiedFile: PATH.resolve(__dirname, '../app/bundle.js'),
+  sourceMap: ROOT(PATH.resolve(__dirname, './../')) + '/app/bundle.js.map',
+  minifiedFile: ROOT(PATH.resolve(__dirname, './../')) + '/app/bundle.js',
   overwrite: 'true'
-}, (err) => {
+}
+
+console.log(`BUGSNAG: uploading sourcemap for v${PROJECT.version}`);
+
+UPLOAD(OPTIONS, (err) => {
   if (err) {
     throw new Error(`BUGSNAG: Something went wrong - ${err.message}`);
   }
   console.log('BUGSNAG: Sourcemap was uploaded successfully.');
-}, "https://upload-bugsnag.contactlab.it/");
+});
