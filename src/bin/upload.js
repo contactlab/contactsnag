d38b682f683ef1b3f5e1b6438ecf2f3f88b7636c
StefanoMagrassi <1291271+StefanoMagrassi@users.noreply.github.com>
2019-03-07 14:40:03 +0100

    typescript project configuration

--- src/bin/upload.js
+++ src/bin/upload.js
@@ -2,11 +2,11 @@
 
 /*eslint no-console: 0*/
 
-const path              = require('path');
-const root              = require('find-root');
+const path = require('path');
+const root = require('find-root');
 const bugsnagSourcemaps = require('bugsnag-sourcemaps');
 
-const ROOT           = root(path.join(__dirname, '../..'));
+const ROOT = root(path.join(__dirname, '../..'));
 const DEFAULT_BUNDLE = 'app/bundle.js';
 
 const pkg = require(path.join(ROOT, 'package.json'));
@@ -16,11 +16,11 @@ const {apiKey, minifiedUrl, sourceMap, minifiedFile} = bugsnag;
 const OPTIONS = {
   apiKey,
   minifiedUrl,
-  endpoint    : 'https://upload-bugsnag.contactlab.it/',
-  appVersion  : version,
-  sourceMap   : sourceMap || path.join(ROOT, `${DEFAULT_BUNDLE}.map`),
+  endpoint: 'https://upload-bugsnag.contactlab.it/',
+  appVersion: version,
+  sourceMap: sourceMap || path.join(ROOT, `${DEFAULT_BUNDLE}.map`),
   minifiedFile: minifiedFile || path.join(ROOT, DEFAULT_BUNDLE),
-  overwrite   : 'true'
+  overwrite: 'true'
 };
 
 // --- Run command
@@ -29,4 +29,6 @@ console.log(`BUGSNAG: uploading sourcemap for v${version}`);
 bugsnagSourcemaps
   .upload(OPTIONS)
   .then(() => console.log('BUGSNAG: Sourcemap was uploaded successfully.'))
-  .catch(err => console.error(`BUGSNAG: Something went wrong - ${err.message}`));
+  .catch(err =>
+    console.error(`BUGSNAG: Something went wrong - ${err.message}`)
+  );
