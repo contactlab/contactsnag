1d916d43bc730964c84bb0d5cad6d0d68afa402c
StefanoMagrassi <1291271+StefanoMagrassi@users.noreply.github.com>
2019-07-11 14:50:44 +0200

    simplify trace

--- test/trace.spec.ts
+++ test/trace.spec.ts
@@ -1,18 +1,14 @@
+import chalk from 'chalk';
+import {trace} from '../src/bin/trace';
 import {result} from './_helpers';
 
-import chalk from 'chalk';
-import {TaskEither, taskEither} from 'fp-ts/lib/TaskEither';
-import {withTrace} from '../src/bin/trace';
-
-test('withTrace() should apply an effectful operation with provided data logging a message to the console', () => {
+test('trace() should log a message to the console and return it', () => {
   const spy = jest.spyOn(console, 'info').mockImplementation(() => undefined);
-  const msg = (a: string): string => `MSG: ${a}`;
-  const effect = (a: string): TaskEither<Error, string> => taskEither.of(a);
 
-  return result(withTrace('TEST', msg, effect), data => {
+  return result(trace('TEST'), data => {
     expect(data.isRight()).toBe(true);
     expect(data.value).toBe('TEST');
     expect(spy).toBeCalledTimes(1);
-    expect(spy).toBeCalledWith(chalk.cyanBright('\n> MSG: TEST'));
+    expect(spy).toBeCalledWith(chalk.cyanBright('\n> TEST'));
   });
 });
