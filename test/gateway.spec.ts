909ea274a2e9a3c710ab4b0769332befdd3b93eb
StefanoMagrassi <1291271+StefanoMagrassi@users.noreply.github.com>
2019-07-11 14:52:08 +0200

    rename exported gateway function

--- test/gateway.spec.ts
+++ test/gateway.spec.ts
@@ -1,10 +1,10 @@
-import {program} from '../src/bin/gateway';
+import {gateway} from '../src/bin/gateway';
 import {result} from './_helpers';
 
 test('program() should return the first command from arguments', () => {
-  const command1 = program(['upload', 'report']);
-  const command2 = program(['args', 'and', 'report', 'and', 'other']);
-  const command3 = program(['report', 'upload', '--tail']);
+  const command1 = gateway(['upload', 'report']);
+  const command2 = gateway(['args', 'and', 'report', 'and', 'other']);
+  const command3 = gateway(['report', 'upload', '--tail']);
 
   return expect(
     Promise.all([result(command1), result(command2), result(command3)])
@@ -12,6 +12,6 @@ test('program() should return the first command from arguments', () => {
 });
 
 test('program() should fail if no available command in arguments', () =>
-  expect(result(program(['args', 'and', 'other', '--tail']))).rejects.toEqual(
+  expect(result(gateway(['args', 'and', 'other', '--tail']))).rejects.toEqual(
     new Error('Use one of available commands: report | upload')
   ));
